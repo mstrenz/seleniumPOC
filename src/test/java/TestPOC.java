a24556076c4ebe7b049ffaf9bfbf3bb919c25c10
Matt Strenz <matt.strenz@slalom.com>
2019-02-01 08:30:49 -0700

    fix for working with gitlab and updated junit, gradle, and selenium

--- src/test/java/TestPOC.java
+++ src/test/java/TestPOC.java
@@ -1,34 +1,48 @@
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.remote.RemoteWebDriver;
 
-import static org.junit.Assert.assertEquals;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import java.io.File;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 public class TestPOC{
+    private static ChromeDriverService service;
     public static RemoteWebDriver testDriver;
 
 
-    @BeforeClass
+    @BeforeAll
     public static void setup(){
         System.setProperty("webdriver.chrome.driver", "lib/chromedriver");
-        testDriver = new ChromeDriver();
+        //Configure Chromedriver
+        service = new ChromeDriverService.Builder()
+                .usingDriverExecutable(new File("lib/chromedriver"))
+                .usingAnyFreePort()
+                .build();
+        try {
+            service.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //Get Driver
+        ChromeOptions options = new ChromeOptions();
+        testDriver = new RemoteWebDriver(service.getUrl(), options);
         testDriver.get("https://www.google.com");
     }
 
-    @Before
-    public void clean(){
-        testDriver.manage().deleteAllCookies();
-    }
 
     @Test
     public void testOne(){
         assertEquals("https://www.google.com/", testDriver.getCurrentUrl());
     }
 
-    @AfterClass
+    @AfterAll
     public static void tearDown(){
         testDriver.quit();
     }
